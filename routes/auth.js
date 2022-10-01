52f07f5f9ec6305ac054cd8cc1c8044eab1b4c14
Cecil D'Costa (Admin Account) <adm_cd@resthaven.asn.au>
2022-10-01 19:00:46 +0930

    logout issue resolved

--- routes/auth.js
+++ routes/auth.js
@@ -9,15 +9,22 @@ router.get('/google',passport.authenticate('google', {scope: ['profile']}) )
 
 
 //Dashboards page
-router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/' }), (req, res) => {res.redirect('/dashboard')})
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/' }), (req, res) => {res.redirect('/dashboard')});
 
 
 //@desc Logout user
 //@route /auth/logout
 
-router.get('/logout', (res, req)=> {
-    req.logout();
-    res.redirect('/');
+router.get('/logout', (req, res)=> {
+    //req.logout();
+    //res.redirect('/');
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+        
+
+
 })
 
 
