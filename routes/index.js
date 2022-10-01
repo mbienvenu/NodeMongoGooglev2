f238de25bbbd457f586b139258644710d71c5e56
Cecil D'Costa (Admin Account) <adm_cd@resthaven.asn.au>
2022-10-01 20:30:15 +0930

    logout issue resolved

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require('/middleware/auth')
 
 // desc: Login/landing pages
 // GET /
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest ,  (req, res) => {
     res.render('login', {layout: 'login'} )
 })
 
 
 //Dashboards page
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth ,(req, res) => {
     res.render('dashboard')
 })
 
